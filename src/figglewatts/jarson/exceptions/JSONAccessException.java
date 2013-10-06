f7a153028ec1132af107aeab0aab221750bf1ae4
Figglewatts <figglewatts@hotmail.co.uk>
2013-10-06 16:16:29 +0100

    Added JSONAccessException for exceptions when accessing a Node's children.

---
+++ src/figglewatts/jarson/exceptions/JSONAccessException.java
@@ -0,0 +1,9 @@
+package figglewatts.jarson.exceptions;
+
+public class JSONAccessException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public JSONAccessException(String message) {
+		super(message);
+	}
+}

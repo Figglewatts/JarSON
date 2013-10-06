a2a1a59d127f7e035a51e070f1c70b11c101cd3c
Figglewatts <figglewatts@hotmail.co.uk>
2013-10-06 11:33:20 +0100

    Added JSONParseException.java, which is thrown during any exceptions encountered whilst parsing a JSON string.

---
+++ src/figglewatts/jarson/exceptions/JSONParseException.java
@@ -0,0 +1,9 @@
+package figglewatts.jarson.exceptions;
+
+public class JSONParseException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public JSONParseException(String message) {
+		super(message);
+	}
+}

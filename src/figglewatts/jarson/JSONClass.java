e7b2170f8a7d6961f8881c29559174afd168e56e
Figglewatts <figglewatts@hotmail.co.uk>
2013-10-06 19:29:44 +0100

    Added some basic functionality to JSONClass.java.

--- src/figglewatts/jarson/JSONClass.java
+++ src/figglewatts/jarson/JSONClass.java
@@ -1,5 +1,36 @@
 package figglewatts.jarson;
 
-public class JSONClass extends JSONNode {
+import java.util.HashMap;
+import java.util.Map;
 
+import figglewatts.jarson.exceptions.JSONAccessException;
+
+public class JSONClass extends JSONNode {
+	private Map<String, JSONNode> children = new HashMap<String, JSONNode>();
+	
+	@Override
+	public void setNode(JSONNode node, String key) throws JSONAccessException {
+		if (children.containsKey(key)) {
+			children.put(key, node);
+		} else {
+			String exceptionString = "";
+			String.format("Child with key {0} does not exist!", key);
+			throw new JSONAccessException(exceptionString);
+		}
+	}
+	@Override
+	public JSONNode getNode(String key) throws JSONAccessException {
+		if (children.containsKey(key)) {
+			return children.get(key);
+		} else {
+			String exceptionString = "";
+			String.format("Child with key {0} does not exist!", key);
+			throw new JSONAccessException(exceptionString);
+		}
+	}
+	
+	@Override
+	public int size() {
+		return children.size();
+	}
 }

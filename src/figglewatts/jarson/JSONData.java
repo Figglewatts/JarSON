9923c3a3a59aef60e866683aea80ef6acecceeb6
Figglewatts <figglewatts@hotmail.co.uk>
2013-10-13 18:53:06 +0100

    Added initial functionality to JSONData.java

--- src/figglewatts/jarson/JSONData.java
+++ src/figglewatts/jarson/JSONData.java
@@ -1,5 +1,19 @@
 package figglewatts.jarson;
 
 public class JSONData extends JSONNode {
-
+	private String data;
+	
+	@Override
+	public String getValue() {
+		return data;
+	}
+	
+	@Override
+	public void setValue(String data) {
+		this.data = data;
+	}
+	
+	public JSONData(String data) {
+		this.data = data;
+	}
 }

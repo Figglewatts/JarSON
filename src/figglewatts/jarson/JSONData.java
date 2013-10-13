a1257b7d979c3371e1a5fde5f974d201c7885610
Figglewatts <figglewatts@hotmail.co.uk>
2013-10-13 19:26:39 +0100

    Added more constructors to JSONData.java to better make use of the previously added typecasting properties in JSONNode.java Added overriding toString() methods in JSONData.java

--- src/figglewatts/jarson/JSONData.java
+++ src/figglewatts/jarson/JSONData.java
@@ -16,4 +16,28 @@ public class JSONData extends JSONNode {
 	public JSONData(String data) {
 		this.data = data;
 	}
+	public JSONData(int data) {
+		this.setAsInt(data);
+	}
+	public JSONData(float data) {
+		this.setAsFloat(data);
+	}
+	public JSONData(double data) {
+		this.setAsDouble(data);
+	}
+	public JSONData(boolean data) {
+		this.setAsBoolean(data);
+	}
+	
+	@Override
+	public String toString() {
+		return "\"" + Escape(data) + "\"";
+	}
+	
+	@Override
+	public String toString(String prefix) {
+		return "\"" + Escape(data) + "\"";
+	}
+	
+	// TODO: add serialize() method
 }

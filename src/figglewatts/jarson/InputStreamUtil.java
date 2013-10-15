785fe8e6bca6c7b19697d666c9a00fa1e955d1ee
Figglewatts <figglewatts@hotmail.co.uk>
2013-10-15 20:05:18 +0100

    Added newline/carraige return checking to ReadString().

--- src/figglewatts/jarson/InputStreamUtil.java
+++ src/figglewatts/jarson/InputStreamUtil.java
@@ -19,6 +19,11 @@ public class InputStreamUtil {
 		StringBuilder builder = new StringBuilder();
 		int ch;
 		while ((ch = in.read()) != -1) {
+			// 0x0D is carraige return
+			// 0x0A is line feed
+			if (ch == 0x0D || ch == 0x0A) {
+				break; // newline or carraige return has been found, break out of the loop
+			}
 			builder.append((char)ch);
 		}
 		return builder.toString();

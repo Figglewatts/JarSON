a5223f68400d3149834c78378e0a79791826c3b1
Figglewatts <figglewatts@hotmail.co.uk>
2017-03-31 13:58:59 +0100

    I am rewriting this library, deleted all files

--- src/figglewatts/jarson/exceptions/JSONAccessException.java
+++
@@ -1,9 +0,0 @@
-package figglewatts.jarson.exceptions;
-
-public class JSONAccessException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public JSONAccessException(String message) {
-		super(message);
-	}
-}
